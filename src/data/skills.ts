cc15dc1c7e17873848eae43f7535145c22b440dc
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-06-27 12:49:12 +0700

    feat: add type definition and data project

---
+++ src/data/skills.ts
@@ -0,0 +1,35 @@
+import type { Skill } from '../types/skill';
+
+export const skills: Skill[] = [
+  {
+    name: 'React',
+    category: 'frontend',
+  },
+  {
+    name: 'TypeScript',
+    category: 'frontend',
+  },
+  {
+    name: 'Tailwind CSS',
+    category: 'frontend',
+  },
+
+  {
+    name: 'NestJS',
+    category: 'backend',
+  },
+  {
+    name: 'Express',
+    category: 'backend',
+  },
+
+  {
+    name: 'MongoDB',
+    category: 'database',
+  },
+
+  {
+    name: 'Git',
+    category: 'tools',
+  },
+];
