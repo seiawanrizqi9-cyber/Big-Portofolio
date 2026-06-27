eaf645a8a6bf84048d7d30657ab74013a3845d10
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-06-27 16:34:36 +0700

    feat: add project section layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Hero from "../components/sections/Hero/Hero";
 import About from "../components/sections/About/about";
 import Skills from "../components/sections/Skills/Skills";
+import Projects from "../components/sections/Projects/Projects";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Hero />
       <About />
       <Skills />
+      <Projects />
     </>
   );
 };
